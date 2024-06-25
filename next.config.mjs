73c0fa212d44ee755b057481ed3e5237a20e9c37
felipe300 <caracoloro@gmail.com>
2024-06-24 21:19:59 -0400

    feat: add signin and logout

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    // WARNING: not recommended, vercel will charge for every "resize image", so create your own resize function
+    remotePatterns: [{ hostname: "lh3.googleusercontent.com" }],
+  },
+};
 
 export default nextConfig;
