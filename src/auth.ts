0451491b5367bc3e090d030711c282a477553342
felipe300 <caracoloro@gmail.com>
2024-07-01 17:33:32 -0400

    feat: add client side auth vs partial render side

--- src/auth.ts
+++ src/auth.ts
@@ -3,11 +3,14 @@ import { PrismaAdapter } from "@auth/prisma-adapter";
 import prisma from "./lib/prisma";
 import Google from "next-auth/providers/google";
 import GitHub from "next-auth/providers/github";
+import { Adapter } from "next-auth/adapters";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
+  // WARNING: this is a bug, in the future you should not use it
+  trustHost: true, // NOTE: OR add to ".env" => AUTH_TRUST_HOST="http://localhost:3000"
   theme: {
     logo: "/logo.png",
   },
-  adapter: PrismaAdapter(prisma),
+  adapter: PrismaAdapter(prisma) as Adapter,
   providers: [Google, GitHub],
 });
